d436bb3f81c983d6f81afa7c7a88601df4e06b33
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-08-21 11:20:11 +0430

    Add files via upload

---
+++ javadoc/wait(), notify() and notifyAll() in Java - A tutorial_files/challenge.js
@@ -0,0 +1,14 @@
+
+var RecaptchaState = {
+    site : '6LfUr8YSAAAAAD7iVx6Cgl7wNp5m9HRfjrQnUk4F',
+    rtl : false,
+    challenge : '03AHJ_VusyUsvQylrhdWqpxBpTN5Zo3IJkSZ7Ww0CcpdBQ9CCEvLL7CkUDqbpUsrfenatAHdzJNEUKi__IV9UrfMH5p6J9u8Ql3CSjd50lmTjMXLrmRbeIWiKZgdE5X5KgNY-M5eO9TITOtW9PAoPyKbiuJ9T1vDUeHg',
+    is_incorrect : false,
+    programming_error : '',
+    error_message : '',
+    server : 'http://www.google.com/recaptcha/api/',
+    lang : 'en',
+    timeout : 1800
+};
+
+document.write('<scr'+'ipt type="text/javascript" s'+'rc="' + RecaptchaState.server + 'js/recaptcha.js"></scr'+'ipt>');
