d18dd95810d237aab4d317c948671fd35411ec1a
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-12-18 21:49:18 +0330

    Add files via upload

---
+++ Lynda Exercise Solutions/03_GettingStarted/APIDocs/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Number of args: " + args.length);
+		
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+		
+	}
+
+}
